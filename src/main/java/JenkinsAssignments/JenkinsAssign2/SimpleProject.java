9c5c48f454c4795325db1436d49a3322ce94c8ff
Geetha <danda.saigeetha@wipro.com>
2024-04-17 04:44:24 +0000

    Intial commit to Jenkins repository

---
+++ src/main/java/JenkinsAssignments/JenkinsAssign2/SimpleProject.java
@@ -0,0 +1,10 @@
+package JenkinsAssignments.JenkinsAssign2;
+
+public class SimpleProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
